01a3d7e531c45542d8caea2679b70debe11a07f2
Ernest <Ernest@Er.com>
2022-11-24 07:57:37 +0500

    p6

--- 3_lab_3_sem/Form1.cs
+++ 3_lab_3_sem/Form1.cs
@@ -10,11 +10,32 @@ using System.Windows.Forms;
 
 namespace _3_lab_3_sem
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Form, IController
     {
+        static Random r = new Random();
+        List<IView> views = new List<IView>();
+        IModel model;
+        IModel IController.Model 
+        { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void AddView(IView view)
+        {
+            views.Add(view);
+        }
+
+        public void Add()
+        {
+            model.addNode(r.Next(100));
+        }
+
+        public void Remove()
+        {
+            model.RemoveLastNode();
         }
     }
 }
